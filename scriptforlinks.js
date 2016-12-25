2af6dbb09244dc8f496ff439b2ee1ca50771f0e1
Kapil Peraka <kapil.peraka@gmail.com>
2016-12-25 10:02:30 -0800

    Update scriptforlinks.js

--- scriptforlinks.js
+++ scriptforlinks.js
@@ -1,11 +1,27 @@
-function getSrcLink() {
-return "http://www.anrdoezrs.net/placeholder-24755168?target=_top&mouseover=N";              
+function getSrcLinkLeader() {
+  var leaderArray = ["http://www.anrdoezrs.net/placeholder-24755168?target=_blank&mouseover=N",
+                "http://www.dpbolvw.net/placeholder-24760820?target=_blank&mouseover=N",
+                "http://www.anrdoezrs.net/placeholder-24699103?target=_blank&mouseover=N",
+                "http://www.tkqlhce.com/placeholder-24699053?target=_blank&mouseover=N"];
+  return leaderArray[Math.floor(Math.random() * leaderArray.length)];
 }
 
-function displayContent() {
-var script = document.createElement("script");
-script.language="javascript"
-script.src = getSrcLink();
-document.write(script.outerHTML);
+function getSrcLinkMobile() {
+  var leaderArray = ["http://www.anrdoezrs.net/placeholder-24760828?target=_blank&mouseover=N",
+                "http://www.dpbolvw.net/placeholder-24699106?target=_blank&mouseover=N",
+                "http://www.kqzyfj.com/placeholder-24760831?target=_blank&mouseover=N",
+                "http://www.kqzyfj.com/placeholder-24698576?target=_blank&mouseover=N"];
+  return leaderArray[Math.floor(Math.random() * leaderArray.length)];
+}
+
+function displayBodyContent() {
+  var script = document.createElement("script");
+  script.language="javascript";
+  if(screen.width < 600) {
+    script.src = getSrcLinkMobile();
+  } else {
+    script.src = getSrcLinkLeader();
+  }
+  document.write(script.outerHTML);
 }
 
