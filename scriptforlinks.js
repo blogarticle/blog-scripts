4fb478c1b6a5d4a72ff2724048e2f8e7f91c157b
Kapil Peraka <kapil.peraka@gmail.com>
2016-12-25 08:58:45 -0800

    Update scriptforlinks.js

--- scriptforlinks.js
+++ scriptforlinks.js
@@ -1,6 +1,6 @@
 <script>
 function getSrcLink() {
 	//nothing additional here
-	return "http://www.anrdoezrs.net/placeholder-24755168?target=_top&mouseover=N";              
+	return 'http://www.anrdoezrs.net/placeholder-24755168?target=_top&mouseover=N';              
 }
 </script>
