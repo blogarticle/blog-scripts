702666d795b2aa1eb503a80851d61ef7b9ac2193
Kapil Peraka <kapil.peraka@gmail.com>
2016-12-28 08:27:15 -0800

    Update testing-script.js

--- testing-script.js
+++ testing-script.js
@@ -1,32 +1,42 @@
 //--------------- THIS CODE IS JUST FOR TESTING. MAIN ONE IS OTHER FILE ---------------//
 
-//don't forget to remove the wine ads on december 31 (2 items just above last item)
-//this is 728x90
+//don't forget to remove the wine ads on december 31 (item just above last item)
+//this is 728x90 size banner
 function getSrcLinkLeader() {
   var leaderArray = ["http://www.anrdoezrs.net/placeholder-24755168?target=_blank&mouseover=N",
                      "http://www.tkqlhce.com/placeholder-24699053?target=_blank&mouseover=N"];                
   return leaderArray[Math.floor(Math.random() * leaderArray.length)];
 }
 
-//this is 300x250
+//this is 300x250 size banner
 function getSrcLinkMobile() {
   var mobileArray = ["http://www.anrdoezrs.net/placeholder-24760828?target=_blank&mouseover=N",
                      "http://www.kqzyfj.com/placeholder-24760862?target=_blank&mouseover=N"];
   return mobileArray[Math.floor(Math.random() * mobileArray.length)];
 }
 
-//don't forget to remove the wine ads on december 31 (2 items just above last item)
-//this is 160x600
+//don't forget to remove the wine ads on december 31 (item just above last item)
+//this is 160x600 size banner
 function getSrcLinkSideBanner() {
   var sideBannerArray = ["http://www.jdoqocy.com/placeholder-24698462?target=_blank&mouseover=N",
                          "http://www.dpbolvw.net/placeholder-24761074?target=_blank&mouseover=N"];
   return sideBannerArray[Math.floor(Math.random() * sideBannerArray.length)];
 }
 
-function getProductIDs(){
-  return "B0001HLTSE,B002WUPWC2,B00CGSES9I,B00O4XR5QQ,B00FR85IRK,B00EWCUK1Q,B01HL8LXQ8";
+//for displaying amazon recommendations ad
+function displayEndRecomm() {
+  var node = document.createElement("div");
+  node.style = "width:98%";
+  //create script body
+  var script = document.createElement("script");
+  script.language="javascript";
+  script.src = "//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=d08216e9-ee4d-4362-b467-952cb396ecf9&storeId=kperaka-20";
+  //append script to node
+  node.appendChild(script);
+  document.write(node.outerHTML);
 }
 
+//for displaying the ad in the body
 function displayBodyContent() {
   var script = document.createElement("script");
   script.language="javascript";
@@ -38,6 +48,7 @@ function displayBodyContent() {
   document.write(script.outerHTML);
 }
 
+//for displaying mobile only ad
 function displayMobileContent() {
   if(screen.width < 500) {
     var script = document.createElement("script");
@@ -47,6 +58,7 @@ function displayMobileContent() {
   } 
 }
 
+//for displaying side banner
 function displaySideBannerContent() {
   if(screen.width > 500) {
     var script = document.createElement("script");
