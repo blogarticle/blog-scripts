1ede0c4d6817d3fe122c1b3455b0678cae67e10b
Kapil Peraka <kapil.peraka@gmail.com>
2017-01-09 14:13:19 -0800

    Update scriptforlinks.js

--- scriptforlinks.js
+++ scriptforlinks.js
@@ -2,29 +2,29 @@
 //this is 728x90 size banner for travel and others
 function getSrcLinkLeader() {
   var leaderArray = ["http://www.anrdoezrs.net/placeholder-24755168?target=_blank&mouseover=N",
-                     "http://www.anrdoezrs.net/placeholder-24900980?target=_blank&mouseover=N",
-                     "http://www.jdoqocy.com/placeholder-24882799?target=_blank&mouseover=N",
-                     "http://www.kqzyfj.com/placeholder-24872997?target=_blank&mouseover=N",
-                     "http://www.tkqlhce.com/placeholder-24872967?target=_blank&mouseover=N",
-                     "http://www.kqzyfj.com/placeholder-24872974?target=_blank&mouseover=N",
-                     "http://www.tkqlhce.com/placeholder-24799004?target=_blank&mouseover=N",
-                     "http://www.dpbolvw.net/placeholder-24786320?target=_blank&mouseover=N",
-                     "http://www.dpbolvw.net/placeholder-24760820?target=_blank&mouseover=N",
-                     "http://www.dpbolvw.net/placeholder-24900984?target=_blank&mouseover=N",
-                     "http://www.anrdoezrs.net/placeholder-24825200?target=_blank&mouseover=N",
-                     "http://www.tkqlhce.com/placeholder-24699053?target=_blank&mouseover=N",
-                     //non-travel stuff
-                     "http://www.kqzyfj.com/placeholder-24807915?target=_blank&mouseover=N",
-                     "http://www.jdoqocy.com/placeholder-24882820?target=_blank&mouseover=N",
-                     "http://www.tkqlhce.com/placeholder-24872981?target=_blank&mouseover=N",
-                     "http://www.jdoqocy.com/placeholder-24792140?target=_blank&mouseover=N",
-                     "http://www.anrdoezrs.net/placeholder-24784213?target=_blank&mouseover=N",
-                     "http://www.anrdoezrs.net/placeholder-24728885?target=_blank&mouseover=N",
-                     "http://www.jdoqocy.com/placeholder-24760848?target=_blank&mouseover=N",
-                     "http://www.dpbolvw.net/placeholder-24760854?target=_blank&mouseover=N",
-                     "http://www.jdoqocy.com/placeholder-24712954?target=_blank&mouseover=N",
-                     "http://www.tkqlhce.com/placeholder-24773817?target=_blank&mouseover=N",
-                     "http://www.jdoqocy.com/placeholder-24825190?target=_blank&mouseover=N"];                
+  "http://www.anrdoezrs.net/placeholder-24900980?target=_blank&mouseover=N",
+  "http://www.jdoqocy.com/placeholder-24882799?target=_blank&mouseover=N",
+  "http://www.kqzyfj.com/placeholder-24872997?target=_blank&mouseover=N",
+  "http://www.tkqlhce.com/placeholder-24872967?target=_blank&mouseover=N",
+  "http://www.kqzyfj.com/placeholder-24872974?target=_blank&mouseover=N",
+  "http://www.tkqlhce.com/placeholder-24799004?target=_blank&mouseover=N",
+  "http://www.dpbolvw.net/placeholder-24786320?target=_blank&mouseover=N",
+  "http://www.dpbolvw.net/placeholder-24760820?target=_blank&mouseover=N",
+  "http://www.dpbolvw.net/placeholder-24900984?target=_blank&mouseover=N",
+  "http://www.anrdoezrs.net/placeholder-24825200?target=_blank&mouseover=N",
+  "http://www.tkqlhce.com/placeholder-24699053?target=_blank&mouseover=N",
+  //non-travel stuff
+  "http://www.kqzyfj.com/placeholder-24807915?target=_blank&mouseover=N",
+  "http://www.jdoqocy.com/placeholder-24882820?target=_blank&mouseover=N",
+  "http://www.tkqlhce.com/placeholder-24872981?target=_blank&mouseover=N",
+  "http://www.jdoqocy.com/placeholder-24792140?target=_blank&mouseover=N",
+  "http://www.anrdoezrs.net/placeholder-24784213?target=_blank&mouseover=N",
+  "http://www.anrdoezrs.net/placeholder-24728885?target=_blank&mouseover=N",
+  "http://www.jdoqocy.com/placeholder-24760848?target=_blank&mouseover=N",
+  "http://www.dpbolvw.net/placeholder-24760854?target=_blank&mouseover=N",
+  "http://www.jdoqocy.com/placeholder-24712954?target=_blank&mouseover=N",
+  "http://www.tkqlhce.com/placeholder-24773817?target=_blank&mouseover=N",
+  "http://www.jdoqocy.com/placeholder-24825190?target=_blank&mouseover=N"];                
   return leaderArray[Math.floor(Math.random() * leaderArray.length)];
 }
 
@@ -32,28 +32,50 @@ function getSrcLinkLeader() {
 //this is 300x250 size banner for travel
 function getSrcLinkMobile() {
   var mobileArray = ["http://www.anrdoezrs.net/placeholder-24760828?target=_blank&mouseover=N",
-                     "http://www.tkqlhce.com/placeholder-24900981?target=_blank&mouseover=N",
-                     "http://www.anrdoezrs.net/placeholder-24882786?target=_blank&mouseover=N",
-                     "http://www.kqzyfj.com/placeholder-24882804?target=_blank&mouseover=N",
-                     "http://www.dpbolvw.net/placeholder-24882795?target=_blank&mouseover=N",
-                     "http://www.tkqlhce.com/placeholder-24872944?target=_blank&mouseover=N",
-                     "http://www.jdoqocy.com/placeholder-24872951?target=_blank&mouseover=N",
-                     "http://www.kqzyfj.com/placeholder-24786332?target=_blank&mouseover=N",
-                     "http://www.tkqlhce.com/placeholder-24699106?target=_blank&mouseover=N",
-                     "http://www.jdoqocy.com/placeholder-24900985?target=_blank&mouseover=N",
-                     "http://www.jdoqocy.com/placeholder-24825199?target=_blank&mouseover=N",
-                     "http://www.kqzyfj.com/placeholder-24760862?target=_blank&mouseover=N",
-                     //non-travel stuff
-                     "http://www.jdoqocy.com/placeholder-24807910?target=_blank&mouseover=N",
-                     "http://www.jdoqocy.com/placeholder-24882822?target=_blank&mouseover=N",
-                     "http://www.tkqlhce.com/placeholder-24872992?target=_blank&mouseover=N",
-                     "http://www.anrdoezrs.net/placeholder-24792142?target=_blank&mouseover=N",
-                     "http://www.tkqlhce.com/placeholder-24761239?target=_blank&mouseover=N",
-                     "http://www.dpbolvw.net/placeholder-24728890?target=_blank&mouseover=N",
-                     "http://www.dpbolvw.net/placeholder-24760867?target=_blank&mouseover=N",
-                     "http://www.kqzyfj.com/placeholder-24760868?target=_blank&mouseover=N",
-                     "http://www.kqzyfj.com/placeholder-24712934?target=_blank&mouseover=N"];
+  "http://www.tkqlhce.com/placeholder-24900981?target=_blank&mouseover=N",
+  "http://www.anrdoezrs.net/placeholder-24882786?target=_blank&mouseover=N",
+  "http://www.kqzyfj.com/placeholder-24882804?target=_blank&mouseover=N",
+  "http://www.dpbolvw.net/placeholder-24882795?target=_blank&mouseover=N",
+  "http://www.tkqlhce.com/placeholder-24872944?target=_blank&mouseover=N",
+  "http://www.jdoqocy.com/placeholder-24872951?target=_blank&mouseover=N",
+  "http://www.kqzyfj.com/placeholder-24786332?target=_blank&mouseover=N",
+  "http://www.tkqlhce.com/placeholder-24699106?target=_blank&mouseover=N",
+  "http://www.jdoqocy.com/placeholder-24900985?target=_blank&mouseover=N",
+  "http://www.jdoqocy.com/placeholder-24825199?target=_blank&mouseover=N",
+  "http://www.kqzyfj.com/placeholder-24760862?target=_blank&mouseover=N",
+  //non-travel stuff
+  "http://www.jdoqocy.com/placeholder-24807910?target=_blank&mouseover=N",
+  "http://www.jdoqocy.com/placeholder-24882822?target=_blank&mouseover=N",
+  "http://www.tkqlhce.com/placeholder-24872992?target=_blank&mouseover=N",
+  "http://www.anrdoezrs.net/placeholder-24792142?target=_blank&mouseover=N",
+  "http://www.tkqlhce.com/placeholder-24761239?target=_blank&mouseover=N",
+  "http://www.dpbolvw.net/placeholder-24728890?target=_blank&mouseover=N",
+  "http://www.dpbolvw.net/placeholder-24760867?target=_blank&mouseover=N",
+  "http://www.kqzyfj.com/placeholder-24760868?target=_blank&mouseover=N",
+  "http://www.kqzyfj.com/placeholder-24712934?target=_blank&mouseover=N"];
   return mobileArray[Math.floor(Math.random() * mobileArray.length)];
+}
+
+//this is 728x90 size banner for amazon
+function getAmazonSrcLeader() {
+  var aAmazonLeader = ["//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ur1&category=primemain&banner=1PREMK5A0BD4VB6F8Y02&f=ifr&linkID=1327d9beeba5d66d7a181862980370c0&t=kperaka-20&tracking_id=kperaka-20",
+  "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ur1&category=primegift&banner=0DHKFFV9MZ6GTWBFZK82&f=ifr&linkID=64142d78388da482c07b57b1c6fa8d55&t=kperaka-20&tracking_id=kperaka-20",
+  "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ur1&category=pantry&banner=19HH5TF0WG32NR2GN682&f=ifr&linkID=1016e60cb3d1ec6eddbb747a7cd34b8c&t=kperaka-20&tracking_id=kperaka-20",
+  "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ur1&category=primemusic&banner=1RY2Z0V0AAZV4RSZEXG2&f=ifr&linkID=11dbb5a04003d0af50b0c530e7894328&t=kperaka-20&tracking_id=kperaka-20",
+  "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ur1&category=gift_certificates&banner=1G274HKHXM7QERC7YAG2&f=ifr&linkID=705514b6647d005d0b4bc9845b99fe67&t=kperaka-20&tracking_id=kperaka-20",
+  "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ur1&category=primeent&banner=13T0PEVEM3A4VST18J82&f=ifr&linkID=762f40790cd3b2a88f0f448832692e6e&t=kperaka-20&tracking_id=kperaka-20"];
+  return aAmazonLeader[Math.floor(Math.random() * aAmazonLeader.length)];
+}
+
+//this is 300x250 size banner for amazon
+function getAmazonSrcMobile() {
+  var aAmazonMobile = ["//rcm-na.amazon-adsystem.com/e/cm?o=1&p=12&l=ur1&category=primemain&banner=1N7QZYHSKNC8P06S6QG2&f=ifr&linkID=ca1f67b43e8b56ddf3d06ab218032f8c&t=kperaka-20&tracking_id=kperaka-20",
+  "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=12&l=ur1&category=primegift&banner=10BSGCHPCJG0HZN45H82&f=ifr&linkID=72471067e5d0d9a0a71190dd1d41ae6b&t=kperaka-20&tracking_id=kperaka-20",
+  "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=12&l=ur1&category=pantry&banner=1ASYAN92MJDFSDD273R2&f=ifr&linkID=981c51f5be7f8d686e3ab1111c50e694&t=kperaka-20&tracking_id=kperaka-20",
+  "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=12&l=ur1&category=primemusic&banner=1AKPX3FSXCJ64S0YZW02&f=ifr&linkID=a42850b7fed7a2249f5c56eb13a3ed54&t=kperaka-20&tracking_id=kperaka-20",
+  "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=12&l=ur1&category=primeent&banner=1DD22M4986ZTHWQVR8G2&f=ifr&linkID=dd9807496b4a7cbd13448490de7fd7bf&t=kperaka-20&tracking_id=kperaka-20",
+  "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=12&l=ur1&category=gift_certificates&banner=127JF9E4530CSFRCY4R2&f=ifr&linkID=fb1d3b7542da7155ad5123a6c56471dd&t=kperaka-20&tracking_id=kperaka-20"];
+  return aAmazonMobile[Math.floor(Math.random() * aAmazonMobile.length)];
 }
 
 //for displaying the ad in the body
@@ -82,9 +104,13 @@ function displaySideBannerContent() {
   }  
 }
 
-//for displaying responsive ad ad
+//for displaying end ad
 function displayEndRecomm() {
-  displayAdSenseContent("anySize");
+  if(screen.width < 500) {
+	displayAmazonAd("mobileOnly");
+  } else {
+	displayAmazonAd("other");
+  }
 }
 
 //supporting method for displaying adsense content
@@ -126,3 +152,23 @@ function displayAdSenseContent(adSize) {
   (adsbygoogle = window.adsbygoogle || []).push({});
 }
 
+//supporting method for displaying amazon ad content
+function displayAmazonAd(adSize) {
+  var iFrame = document.createElement("iframe");
+  if(adSize == "mobileOnly") {
+	iFrame.setAttribute("src",getAmazonSrcMobile());
+	iFrame.setAttribute("width","300");
+	iFrame.setAttribute("height","250");
+  } else {
+	iFrame.setAttribute("src",getAmazonSrcLeader());
+	iFrame.setAttribute("width","728");
+	iFrame.setAttribute("height","90");
+  }
+  iFrame.setAttribute("scrolling","no");
+  iFrame.setAttribute("border","0");
+  iFrame.setAttribute("marginwidth","0");
+  iFrame.setAttribute("style","border:none;");
+  iFrame.setAttribute("frameborder","0");
+
+  document.write(iFrame.outerHTML);
+}
