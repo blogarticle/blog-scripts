c12c7f1ca313ffc28c9cc64b62ee254196a5ac9c
Kapil Peraka <kapil.peraka@gmail.com>
2017-01-09 14:22:52 -0800

    Update scriptforlinks.js

--- scriptforlinks.js
+++ scriptforlinks.js
@@ -109,7 +109,7 @@ function displayEndRecomm() {
   if(screen.width < 500) {
 	displayAmazonAd("mobileOnly");
   } else {
-	displayAmazonAd("other");
+	displayAdSenseContent("LeaderBoard");
   }
 }
 
